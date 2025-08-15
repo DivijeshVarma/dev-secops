4a578f405229afb445b31e2172c079e9d1eb888e
divijesh <divijesh@divijeshs-Mac-mini.local>
2025-08-15 15:45:11 +0530

    try5

--- scripts/create_binauthz_policy.sh
+++ scripts/create_binauthz_policy.sh
@@ -1,6 +1,6 @@
 #Project variables for project and the GKE environments
 LOCATION=us-central1
-PROJECT_ID=$(gcloud config list --format 'value(core.project)')
+PROJECT_ID=devopscicd-469105
 ATTESTOR_ID=cb-attestor
 GKE_Test_Cluster_Name=test
 GKE_Staging_Cluster_Name=staging
